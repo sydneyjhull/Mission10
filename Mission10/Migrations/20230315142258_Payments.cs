7b085f871aa7f9dfbef0ffc4ac32e4ebbf3230e9
Sydney Hull <sydneyjhull@gmail.com>
2023-03-15 09:03:30 -0600

    done

---
+++ Mission10/Migrations/20230315142258_Payments.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission9.Migrations
+{
+    public partial class Payments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    PaymentId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: false),
+                    AddressLine1 = table.Column<string>(nullable: false),
+                    AddressLine2 = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: false),
+                    Zip = table.Column<string>(nullable: false),
+                    Country = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.PaymentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BasketLineItem",
+                columns: table => new
+                {
+                    LineID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BookId = table.Column<int>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    PaymentId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BasketLineItem", x => x.LineID);
+                    table.ForeignKey(
+                        name: "FK_BasketLineItem_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BasketLineItem_Payments_PaymentId",
+                        column: x => x.PaymentId,
+                        principalTable: "Payments",
+                        principalColumn: "PaymentId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketLineItem_BookId",
+                table: "BasketLineItem",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketLineItem_PaymentId",
+                table: "BasketLineItem",
+                column: "PaymentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BasketLineItem");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
